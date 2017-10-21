ef476b9bfb08c9369ac8327cd2e7f9821e427768
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2017-10-21 05:56:27 -0200

    [FIX] Atualização Inimigos

--- Jogo/Assets/Scripts/Inimigos.cs
+++ Jogo/Assets/Scripts/Inimigos.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inimigos : MonoBehaviour {
+public class Inimigos : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string palavra;
+    public int pontuacao;
+
+    bool acertarLetra;
+
+    //Variáveis para gerar e movimentar inimigos aleatoriamente
+    public float impulsoMaximo;
+    public float rotacaoMaxima;
+    public Rigidbody2D rigidBody2D; 
+
+    // Use this for initialization
+    void Start()
+    {
+        this.palavra = palavra;
+
+        Vector2 impulso = new Vector2(Random.Range(-impulsoMaximo, impulsoMaximo), Random.Range(-impulsoMaximo, impulsoMaximo));
+        float rotacao = Random.Range(-rotacaoMaxima, rotacaoMaxima);
+
+        rigidBody2D.AddForce(impulso);
+        rigidBody2D.AddTorque(rotacao);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+        pontuacao += 10;
+    }
+
 }
