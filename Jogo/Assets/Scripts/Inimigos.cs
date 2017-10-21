dd60142dc08e23c1b53e455c3602e1a7c5c24e6f
accamargo <accamargo@users.noreply.github.com>
2017-10-21 04:54:26 -0200

    [ADD] Não terminado Inimigos

---
+++ Jogo/Assets/Scripts/Inimigos.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigos : MonoBehaviour {
+
+	public string palavra;
+	public int pontuacao;
+
+	bool acertarLetra;
+
+	// Use this for initialization
+	void Start (string palavra) {
+		this.palavra = palavra;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	bool acertouLetra(char letra){
+		int posicaoLetra;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Destroy (this.gameObject);
+		pontuacao += 10;
+	}
+		
+}
