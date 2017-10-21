f8697e6825fc372750d78afaaade7cce746ca122
accamargo <accamargo@users.noreply.github.com>
2017-10-21 17:11:36 -0200

    [ADD] Sistema Pontuação

--- Jogo/Assets/Scripts/CaracteristicasDoJogo.cs
+++ Jogo/Assets/Scripts/CaracteristicasDoJogo.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CaracteristicasDoJogo : MonoBehaviour {
 
     public static int totalVidas;
+	public int pontuacao;
+	public Text pontuacaoText;
 
 	// Use this for initialization
 	void Start () {
         totalVidas = 3;
+		pontuacao = 0;
+		pontuacaoText.text = "Score: " + pontuacao;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void atualizarPontuacao(int novaPontuacao){
+		pontuacao += novaPontuacao;
+		pontuacaoText.text = "Score: " + pontuacao;
+	}
+		
 }
